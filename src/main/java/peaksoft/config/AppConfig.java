6d45b004a982142982dd966e4c1448d8b050e316
Ulukmyrza <ulukmyrza92@gmail.com>
2022-03-06 10:45:37 +0300

    Dog and Timer added

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -1,9 +1,32 @@
 package peaksoft.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import peaksoft.model.AnimalsCage;
+import peaksoft.model.Cat;
+import peaksoft.model.Dog;
+import peaksoft.model.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+    @Bean
+    public AnimalsCage animalsCage (){
+        return  new AnimalsCage();
+    }
+    @Bean
+    public Cat cat(){
+        return new Cat();
+    }
+    @Bean
+    public Dog dog(){
+        return new Dog();
+    }
+    @Bean
+    public Timer timer(){
+        return new Timer();
+    }
+
 }
--- src/main/java/peaksoft/model/Timer.java
+++ src/main/java/peaksoft/model/Timer.java
@@ -1,10 +1,16 @@
 package peaksoft.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
+
     public Long getTime() {
         return nanoTime;
     }
+
+
 }
